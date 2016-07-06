1ff98c4bcd3ceeecf5da6b905f6c5764e1115ab7
Redfish <lesonduko@gmail.com>
2016-07-06 11:25:58 +0200

    ajout du script: ace et des dossiers de cours et du redcookie

---
+++ cours/cours.js
@@ -0,0 +1,30 @@
+//var el = document.getElementById("demo");//
+	//el.addEventListener("click,changeTitleColor()");//
+
+
+
+
+
+	//<script>
+	/*function ma Fct(){
+		var ma_variable = "text";
+		console.log(ma_variable);
+
+	</script>
+	}*/
+
+	//exemple de fonction//
+	function ChangeTitleColor(){
+		
+		document.getElementById("demo").style.color="red";	
+	};
+
+	function ResetColor(){
+
+		document.getElementById("demo").style.color="black";
+
+	};
+
+
+
+	
